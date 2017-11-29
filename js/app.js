ddb2cea449856c759d6d1c728191b392c7b06594
ruthmz <rmatosfer@gmail.com>
2017-11-29 05:41:04 -0500

    agregando variables

--- js/app.js
+++ js/app.js
@@ -9,37 +9,92 @@ var generationAarequipa = Object.keys(generation[0]); // ["2016-2", "2017-1"]
 console.log(generationAarequipa);
 // guarda en un array los objetos de las generaciones que guardan students y ratings
 var genArequipaObject = Object.values(generation[0]); // [{…}, {…}] tiene el valor 2016-2, 2017-1
+var genMexicoObject = Object.values(generation[1]);
+var genLimaObject = Object.values(generation[2]);
+var genSantiagoObject = Object.values(generation[3]);
+
 console.log(genArequipaObject);
+console.log(genMexicoObject);
+console.log(genLimaObject);
+console.log(genSantiagoObject);
+var areStudents= (Object.values(generation[0])[0])["students"]
+//estudiantes y rating de arequipa
+Object.values(generation[0])
+//estudiantes y rating del 2016-2
+Object.values(generation[0])[0]
+
 
 
 var otro = data[sede[2]]['2016-2'];
-// console.log(otro);
+var selectSede = document.getElementById('sede');
+var selectGeneration = document.getElementById('generation');
+var container = document.getElementById('container');
+var AREQUIPA = 'arequipa';
+var LIMA = 'lima';
+var SANTIAGO = 'santiago';
+var MEXICO = 'mexico'
 
-window.addEventListener('load', function() {
-  var selectSede = document.getElementById('sede');
-  var selectGeneration = document.getElementById('generation');
-  selectSede.addEventListener('change', function() {
-    switch (true) {
-    case event.target.value === 'lima':
-      console.log(data[sede[2]]);
-      // deberia tener aca alguna funcion que imprima todos los datos de lima
-      break;
-    case event.target.value === 'arequipa':
-      console.log(data[sede[0]]);
-      break;
-    case event.target.value === 'chile':
-      console.log(data[sede[3]]);
-      break;
-    case event.target.value === 'mexico':
-      console.log(data[sede[1]]);
-      break;
-    };
-  });
-});
-
-function showGeneration(sede, generacion) {
-
+function showPromo(event) {
+  switch (true) {
+  case event.target.value === 'LIM2016-2':
+    // console.log(genLimaObject[0]);
+    showStudents(genLimaObject[0], LIMA)
+    // deberia tener aca alguna funcion que imprima todos los datos de lima
+    break;
+  case event.target.value === 'LIM2017-1':
+    console.log(genLimaObject[1]);
+    break;
+  case event.target.value === 'LIM2017-2':
+    console.log(genLimaObject[2]);
+    break;
+  case event.target.value === 'ARE2016-2':
+  console.log(genArequipaObject[0]);
+  break;
+  case event.target.value === 'ARE2017-1':
+  console.log(genArequipaObject[1]);
+  break;
+  case event.target.value === 'CHILE2016-2':
+  console.log(genSantiagoObject[0]);
+  break;
+  case event.target.value === 'CHILE2017-1':
+  console.log(genSantiagoObject[1]);
+  break;
+  case event.target.value === 'CHILE2017-2':
+  console.log(genSantiagoObject[2]);
+  break;
+  case event.target.value === 'MEX2017-1':
+    console.log(genMexicoObject[0]);
+    break;
+  case event.target.value === 'MEX2017-2':
+  console.log(genMexicoObject[1]);
+  break;
+  }
 }
+selectSede.addEventListener('change', showPromo);
+
+function showStudents(arr, promo) {
+
+  for (var i = 0; i < arr.length; i++)
+    createData(promo, arr[i]);
+}
+
+function createData(promo, nombre) {
+  var element = document.createElement('div');
+  container.appendChild(element)
+
+
+  /* subImagesContainer.appendChild(img); */
+  imagesContainer.appendChild(img);
+}
+
+
+// function info (){
+    
+
+// function showGeneration(sede, generation) {
+//   for(i = 0; i> sede.length; i++)
+// }
+
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 // console.log(data);
